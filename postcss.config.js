f15a8cd89af2326916a35cdeda3d873abe794e68
adrian <adri.latorre@gmail.com>
2021-04-10 16:47:39 +0200

    Upgrade dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,7 @@
-const tailwindcss = require('tailwindcss')
-// const postcssPurgecss = require("@fullhuman/postcss-purgecss");
-const autoprefixer = require('autoprefixer')
-// const postcssImport = require("postcss-import");
-
-// const purgecss = postcssPurgecss({
-//   enabled: false,
-//   content: ["./src/**/*.vue", "./public/**/*.html"],
-//   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-//   whitelistPatterns: [/tab/, /nav/]
-// });
-
+// postcss.config.js
 module.exports = {
-  plugins: [
-    // postcssImport,
-
-    tailwindcss('./tailwind.config.js'),
-    autoprefixer
-    // purgecss
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 }
